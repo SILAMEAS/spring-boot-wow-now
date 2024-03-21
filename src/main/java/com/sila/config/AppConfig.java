81dc140dc89bb2e938b848d015d836849a033e9c
SILAMEAS <sila.meas@allweb.com.kh>
2024-03-21 15:25:53 +0700

    add-port-3030

--- src/main/java/com/sila/config/AppConfig.java
+++ src/main/java/com/sila/config/AppConfig.java
@@ -38,7 +38,7 @@ public class AppConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration cfg=new CorsConfiguration();
-                cfg.setAllowedOrigins(List.of("http://localhost:3000","https://next-js-wow-now.vercel.app"));
+                cfg.setAllowedOrigins(List.of("http://localhost:3000","https://next-js-wow-now.vercel.app","http://localhost:3030"));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
                 cfg.setAllowedHeaders(Collections.singletonList("*"));
